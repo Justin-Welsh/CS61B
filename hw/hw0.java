e8e7d8cc17fe25b0a859b9a200f02a7c81a344a4
Justin <jwelsh518@gmail.com>
2020-08-08 22:50:33 -0400

    Just trying to figure stuff out with threeSum function

--- hw/hw0.java
+++ hw/hw0.java
@@ -24,15 +24,17 @@ public class hw0 {
         int sum = 0;
 
         for(int i = 0; i < a.length-1; i++) {
-            sum += a[i];
+            if(a[i] * 3 == 0) {
+                return a[i];
+            }
         }
 
         return sum;
     }
 
     public static void main(String args[]) {
-        int[] arr = new int[]{1, 2, 3, 4, 5};
+        int[] arr = new int[]{1, 2, 0, 4, 5};
 
-        System.out.println(max(arr));
+        System.out.println(threeSum(arr));
     }
 }
